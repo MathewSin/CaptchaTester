588bd8256f199bf9924c1a8d9e7140d6cbfbe150
Mathew Imanuel <mathew.imanuel.sin@gmail.com>
2024-11-11 13:58:46 +0700

    New

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('button1').addEventListener('click', function() {
+    window.location.href = 'https://mathewsin.github.io/DragCaptcha/';
+  });
+  
+  document.getElementById('button2').addEventListener('click', function() {
+    window.location.href = 'https://mathewsin.github.io/captchaStory2.0/';
+  });
+  
+  document.getElementById('button3').addEventListener('click', function() {
+    window.location.href = 'https://www.example3.com';
+  });
+  
+  document.getElementById('button4').addEventListener('click', function() {
+    window.location.href = 'https://2captcha.com/demo/hcaptcha?difficulty=difficult';
+  });
+
+  document.getElementById('button5').addEventListener('click', function() {
+    window.location.href = 'https://demo.arkoselabs.com/?key=DF9C4D87-CB7B-4062-9FEB-BADB6ADA61E6';
+  });
